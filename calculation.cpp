3b50aee742e27ceb38fe6cfef53309dd3af233bb
Andrei Kazachkov <an.kazachkov3234@gmail.com>
2023-04-02 01:17:46 +0300

    Added some features and fixed bugs:...
    - now you can zoom in on the image without significant loss in quality;
    - added the ability to move images not in AVX format;
    - added README.

--- calculation.cpp
+++ calculation.cpp
@@ -1,10 +1,14 @@
 #include "calculation.hpp"
 
+// #define AVX_ON
+
+// #define DRAW
+
 using namespace sf;
 
 int mandelbrot(void);
 void outputFps(RenderWindow &window, sf::Clock *clock, sf::Text *text);
-void outputMndlbrt(RenderWindow &window);
+void outputMndlbrt(RenderWindow &window, float center_x, float center_y);
 void outputMndlbrtAvx(RenderWindow &window, float center_x, float center_y);
 
 const int SIZE_OF_TEXT = 15;
@@ -29,16 +33,57 @@ int mandelbrot(void)
     Text text;
     text.setFont(font);                  // select the font
     text.setCharacterSize(SIZE_OF_TEXT); // size in pixels
-    text.setFillColor(Color::Blue);
+    text.setFillColor(Color::Red);
 
     float center_x = 0;
     float center_y = 0;
+    float scale    = 1;
 
     Clock clock; // for FPS
+    bool DRAW_MADL = 0;
 
     while (window.isOpen())
     {
         clock.restart();
+
+        if (Keyboard::isKeyPressed(Keyboard::Left))
+        {
+            DRAW_MADL = 1;
+            center_x -= 0.1;
+        } 
+        if (Keyboard::isKeyPressed(Keyboard::Right))
+        {
+            DRAW_MADL = 1;
+            center_x += 0.1;
+        }
+        if (Keyboard::isKeyPressed(Keyboard::Up))
+        {
+            DRAW_MADL = 1;
+            center_y += 0.1;
+        }
+        if (Keyboard::isKeyPressed(Keyboard::Down))
+        {
+            DRAW_MADL = 1;
+            center_y -= 0.1;
+        }   
+        if (Keyboard::isKeyPressed(Keyboard::F1))
+        {
+            DRAW_MADL = 1;
+            BORDER = BORDER*0.8;
+            BORDER = BORDER*0.8;
+            dx = (2*BORDER)/(WINDOW_SIZE_X);
+            dy = (2*BORDER)/(WINDOW_SIZE_Y);
+        }
+            
+        if (Keyboard::isKeyPressed(Keyboard::F2))
+        {
+            DRAW_MADL = 1;
+            BORDER = BORDER*1.25;
+            BORDER = BORDER*1.25;
+            dx = (2*BORDER)/(WINDOW_SIZE_X);
+            dy = (2*BORDER)/(WINDOW_SIZE_Y);
+        }
+            
 
         sf::Event event;                // check all the window's events...
         while (window.pollEvent(event)) // that were triggered since the...
@@ -49,18 +94,22 @@ int mandelbrot(void)
             }
         }
 
-        // outputMndlbrt (window);
-        outputMndlbrtAvx(window, center_x, center_y);
+        #ifdef AVX_ON
+            outputMndlbrtAvx(window, center_x, center_y);
+        
+        #else
+            outputMndlbrt (window, center_x, center_y);
+
+        #endif  
 
         sf::Time elapsedTime = clock.getElapsedTime();
         char fps[100] = {0};
         sprintf(fps, "FPS: %.0f\n", 1 / elapsedTime.asSeconds());
-        printf(fps);
         text.setString(fps);
         window.draw(text);
 
         window.display();
-        window.clear();
+        window.clear(Color::Black);
     }
 
     return 0;
@@ -70,21 +119,21 @@ sf::RectangleShape createRectangle(float width, float height, float x, float y)
 {
     sf::RectangleShape rectangle(sf::Vector2f(120, 50));
     rectangle.setSize(Vector2f(width, height));
-    rectangle.setFillColor(sf::Color::Green);
+    rectangle.setFillColor(Color::Green);
     rectangle.setPosition(x, y);
 
     return rectangle;
 }
 
 
-//                      (or 2*RIGHT_BORDER=6)
+//                      (or 2*BORDER=6)
 //          <------------1200--|----------> 
 //          =============================== |
 //       |  ||             |   |         || |         [   Xc = center_x   ]
 //       |  ||             |   |         || |         [ relative to (0,0) ] new coordinate system
 //       |  ||             |   | .(x0,y0)|| |         [   Yc = center_y   ]
 // y_max |  ||             |   |         || |
-//       |  ||        (0,0)|   |         || 1 (or 2*RIGHT_BORDER=6)
+//       |  ||        (0,0)|   |         || 1 (or 2*BORDER=6)
 //       |  ||-------------|-------------|| 2 --->
 //       |  ||             |   |         || 0
 //   - - - -||- - - - - - -|- -.(Xc,Yc)- || 0 - ->
@@ -102,24 +151,24 @@ void outputMndlbrtAvx(RenderWindow &window, float center_x, float center_y)
 {
     RectangleShape pixel = createRectangle(1, 1, 0, 0);
 
-    __m256 density     = _mm256_set1_ps(200.f);
+    __m256 density     = _mm256_set1_ps(WINDOW_SIZE_X/(2*BORDER));
     __m256 max_rad_vec = _mm256_set1_ps(MAX_DISTANCE);
-    __m256 size_window = _mm256_set1_ps(RIGHT_BORDER);
+    __m256 shift_dx    = _mm256_set_ps (0, dx, 2*dx, 3*dx, 4*dx, 5*dx, 6*dx, 7*dx);
 
-    float x_min = -RIGHT_BORDER - center_x, x_max = RIGHT_BORDER - center_x;
-    float y_min = -RIGHT_BORDER - center_y, y_max = RIGHT_BORDER - center_y;
+    float x_min = -BORDER - center_x, x_max = BORDER - center_x;
+    float y_min = -BORDER - center_y, y_max = BORDER - center_y;
 
     __m256 shift_x = _mm256_set1_ps (-x_min);
     __m256 shift_y = _mm256_set1_ps (y_max);
 
-    for (float y0 = -3.0; y0 <= 3.0; y0 += dy)
+    for (float y0 = -BORDER; y0 <= BORDER; y0 += dy)
     {
         __m256 y0_vec = _mm256_set1_ps(y0);
         __m256 y0_vec_pos = _mm256_mul_ps(_mm256_sub_ps(shift_y, y0_vec), density);
 
-        for (float x0 = -3.0; x0 <= 3.0; x0 += 8*dx)
+        for (float x0 = -BORDER; x0 <= BORDER; x0 += 8*dx)
         {
-            __m256 x0_vec = _mm256_set_ps(x0, x0 + dx, x0 + 2*dx, x0 + 3*dx, x0 + 4*dx, x0 + 5*dx, x0 + 6*dx, x0 + 7*dx);
+            __m256 x0_vec = _mm256_add_ps(_mm256_set1_ps(x0), shift_dx);
             __m256 x0_vec_pos = _mm256_mul_ps(_mm256_add_ps(shift_x, x0_vec), density);
 
             __m256i numIteration = _mm256_set1_epi32(0);
@@ -144,7 +193,7 @@ void outputMndlbrtAvx(RenderWindow &window, float center_x, float center_y)
                 numIteration = _mm256_sub_epi32 (numIteration, _mm256_castps_si256 (cmp_res));
 
                 x = _mm256_add_ps(_mm256_sub_ps(x2_vec, y2_vec), x0_vec); 
-                y = _mm256_add_ps(_mm256_add_ps(xy_vec, xy_vec), y0_vec); // According to this formula counting Real and Imm parts
+                y = _mm256_add_ps(_mm256_add_ps(xy_vec, xy_vec), y0_vec);
             }
 
             uint32_t * iterationArray = (uint32_t *) &numIteration;
@@ -154,31 +203,39 @@ void outputMndlbrtAvx(RenderWindow &window, float center_x, float center_y)
             for (int cntr = 0; cntr < 8; cntr++)
             {
                 pixel.setPosition (xCoord[cntr], yCoord[cntr]);
-                pixel.setFillColor (Color::Green);
+                pixel.setFillColor (Color(255, 242, 245));
                 int n = iterationArray[cntr];
 
                 if (n < MAX_ITERATION)
                 {
                     pixel.setFillColor(Color((uint8_t)n * 30, (uint8_t)n * 5, 255 - (uint8_t)n));
                 }
-
-                window.draw (pixel);
+                
+                #ifdef DRAW
+                    window.draw (pixel);
+                #endif
             }
         }
     }
 }
 
-void outputMndlbrt(RenderWindow &window)
+void outputMndlbrt(RenderWindow &window, float center_x, float center_y)
 {
     RectangleShape pixel = createRectangle(1, 1, 0, 0);
 
-    for (float y0 = -3.0; y0 <= 3.0; y0 += dy)
-    {
-        float y0_pos = (3.0 - y0) * 200; //((float)WINDOW_SIZE_Y/RIGHT_BORDER)
+    float x_min = -BORDER - center_x, x_max = BORDER - center_x;
+    float y_min = -BORDER - center_y, y_max = BORDER - center_y;
 
-        for (float x0 = -3.0; x0 <= 3.0; x0 += dx)
+    float shift_x = BORDER + center_x;
+    float shift_y = BORDER - center_y;
+
+    for (float y0 = -BORDER; y0 <= BORDER; y0 += dy)
+    {
+        float y0_pos = (shift_y - y0) * WINDOW_SIZE_X/(2*BORDER); //((float)WINDOW_SIZE_Y/BORDER)
+
+        for (float x0 = -BORDER; x0 <= BORDER; x0 += dx)
         {
-            float x0_pos = (3.0 + x0) * 200; //((float)WINDOW_SIZE_X/RIGHT_BORDER)
+            float x0_pos = (shift_x + x0) * WINDOW_SIZE_X/(2*BORDER); //((float)WINDOW_SIZE_X/BORDER)
             int numIteration = 0;
 
             for (float x = x0, y = y0; numIteration < MAX_ITERATION; numIteration++)
@@ -197,14 +254,15 @@ void outputMndlbrt(RenderWindow &window)
                 y = xy + xy + y0;
             }
             pixel.setPosition(float(x0_pos), float(y0_pos));
-            pixel.setFillColor(sf::Color((unsigned char)numIteration, 25, (unsigned char)numIteration));
-
+            pixel.setFillColor (Color(255, 242, 245));
             if (numIteration < MAX_ITERATION)
             {
-                pixel.setFillColor(sf::Color::Yellow);
+                pixel.setFillColor(sf::Color((unsigned char)numIteration*30, (unsigned char)numIteration*5, 255 - (unsigned char)numIteration));
             }
 
-            window.draw(pixel);
+            #ifdef DRAW
+                window.draw(pixel);
+            #endif
         }
     }
 }
--- calculation.hpp
+++ calculation.hpp
@@ -11,13 +11,12 @@
 const size_t WINDOW_SIZE_X = 1200;
 const size_t WINDOW_SIZE_Y = 1200;
 
-const float LEFT_BORDER = -3;
-const float RIGHT_BORDER = 3;
+float BORDER = 3;
 
 const size_t MAX_ITERATION = 255;
 const float MAX_DISTANCE = 100.f;
 
-const float dx = 0.005;
-const float dy = 0.005;
+float dx = (2*BORDER)/WINDOW_SIZE_X;
+float dy = (2*BORDER)/WINDOW_SIZE_Y;
 
 #endif
